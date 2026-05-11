49ad1aa52c147930fca0e139db8ca7bead121446
Ashcal9669 <ashcalili@gmail.com>
2026-05-10 20:19:57 -0400

    chore: finalize v1.7.1 versioning and routing fix after rebase

--- package/usr/lib/omv-agent/app.py
+++ package/usr/lib/omv-agent/app.py
@@ -36,7 +36,7 @@ KNOWLEDGE_JSON = os.environ.get(
     "OMV_AGENT_KNOWLEDGE",
     "/usr/share/omv-agent/knowledge/knowledge_base.json"
 )
-VERSION = "1.7.0"
+VERSION = "1.7.1"
 MAX_QUESTION_LEN = 500
 ALLOWED_CONTENT_TYPE = "application/json"
 
