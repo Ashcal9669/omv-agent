bd4a0d88401f57b4f1824b81368b90fa5df0b67a
Ashcal9669 <ashcalili@gmail.com>
2026-05-25 03:20:14 -0400

    Release version 1.7.4 with systemd modifications

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PKG_DIR="$SCRIPT_DIR/package"
 OUT_DIR="$SCRIPT_DIR/dist"
 VER_DIR="$SCRIPT_DIR/versions"
-VERSION="1.7.3"
+VERSION="1.7.4"
 PKG_NAME="openmediavault-agent_${VERSION}_all.deb"
 
 sync_control_version() {
-    sed -i "" "s/^Version:.*/Version: $VERSION/" "$PKG_DIR/DEBIAN/control"
+    sed -i "s/^Version:.*/Version: $VERSION/" "$PKG_DIR/DEBIAN/control"
 }
 
 write_repo_metadata() {
